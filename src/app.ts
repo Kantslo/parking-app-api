b6410eb9d331bd16283322bf3c2e32a5099c97dc
Giorgi Kantsliani <gqancliani@gmail.com>
2023-12-23 00:23:49 +0400

    parking app api

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(process.env.PORT || 3000);
