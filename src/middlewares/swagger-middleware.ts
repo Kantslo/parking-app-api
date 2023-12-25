ca5f206470b658a1e71cbe57d783e641337f0d16
Giorgi Kantsliani <gqancliani@gmail.com>
2023-12-25 16:17:44 +0400

    fix: fixed swaggerMiddleware file

--- src/middlewares/swagger-middleware.ts
+++ src/middlewares/swagger-middleware.ts
@@ -1,5 +1,18 @@
+// import SwaggerUI from "swagger-ui-express";
+// import YAML from "yamljs";
+
+// const options = {
+//   customCss: ".swagger-ui .topbar { display: none }",
+//   customSiteTitle: "Parking App API",
+// };
+
+// const swaggerDocument = YAML.load("./src/config/swagger.yaml");
+// export default [SwaggerUI.serve, SwaggerUI.setup(swaggerDocument, options)];
+
+
 import SwaggerUI from "swagger-ui-express";
 import YAML from "yamljs";
+import cors from "cors"; // Import the cors middleware
 
 const options = {
   customCss: ".swagger-ui .topbar { display: none }",
@@ -7,4 +20,11 @@ const options = {
 };
 
 const swaggerDocument = YAML.load("./src/config/swagger.yaml");
-export default [SwaggerUI.serve, SwaggerUI.setup(swaggerDocument, options)];
+
+// Add cors middleware specifically for Swagger routes
+const swaggerMiddleware = [
+  cors(),
+  ...[SwaggerUI.serve, SwaggerUI.setup(swaggerDocument, options)],
+];
+
+export default swaggerMiddleware;
